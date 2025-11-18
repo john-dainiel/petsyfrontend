9043ac500457aced8fc36443ce1cad856a262a62
john-dainiel <johndainielnachor@gmail.com>
2025-11-19 06:22:55 +0800

    Update admin.js

--- static/Js/admin.js
+++ static/Js/admin.js
@@ -1,4 +1,4 @@
-const backendUrl = "https://petsy-backend.onrender.com";
+const backendUrl = "https://petsy-dow7.onrender.com";
 
 document.addEventListener("DOMContentLoaded", async () => {
   // Load default
@@ -234,3 +234,4 @@ function logout() {
       window.location.href = "login.html";
     });
 }
+
