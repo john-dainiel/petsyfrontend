a15b90fe5ec2e11d4df18ea382c371a884a4265e
john-dainiel <johndainielnachor@gmail.com>
2025-11-19 06:32:20 +0800

    Update community.js

--- static/Js/community.js
+++ static/Js/community.js
@@ -103,8 +103,9 @@ async function addComment(post_id) {
 
 function logout() {
   localStorage.clear();
-  window.location.href = "login.html";
+  window.location.href = "index.html";
 }
 
 document.addEventListener("DOMContentLoaded", loadFeed);
+
 
